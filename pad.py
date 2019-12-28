69805c41e1c581fa5d5355315d7b745754ef2e25
RockatanskyV8 <luccasanttoz@gmail.com>
2019-12-28 16:47:15 -0300

    acrescentando possibilidade de argumentos

--- pad.py
+++ pad.py
@@ -2,29 +2,42 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import sys
-import json
 
-args = sys.argv[1:] #[1]
+args = sys.argv #[1]
+link = (args[-1].split('/'))
 
-link = ''
-for arg in args:
-    link += '/' + arg
-
-source = requests.get('http://dontpad.com' + link).text
-
+source = requests.get('http://dontpad.com/' + args[-1]).text
 soup = BeautifulSoup(source, 'lxml')
 
-file = open(args[-1] + '.txt', 'w')
-file.write(soup.find('textarea', {"id": "text"}).text)
-file.close()
+if args[1] == 'edit':
 
-os.system("nano " + args[-1] + '.txt')
+    file = open(link[-1] + '.txt', 'w')
+    file.write(soup.find('textarea', {"id": "text"}).text)
+    file.close()
 
-file = open(args[-1] + '.txt', 'r')
-raw  = file.read()
+    os.system("nano " + link[-1] + '.txt')
 
-source = requests.post('http://dontpad.com' + link,
-                       headers={ 'Content-type':'application/x-www-form-urlencoded;charset=UTF-8' },
-                       data='text=' + raw )
+    file = open(link[-1] + '.txt', 'r')
+    raw  = file.read()
 
-file.close()
+    requests.post('http://dontpad.com/' + args[-1],
+                   headers={ 'Content-type':'application/x-www-form-urlencoded;charset=UTF-8' },
+                   data='text=' + raw )
+
+    file.close()
+
+elif 'send:' in args[1]:
+    aux = (args[1]).split(':')[1]
+
+    file = open(aux, 'r')
+    raw  = file.read()
+
+    requests.post('http://dontpad.com/' + args[-1],
+                   headers={ 'Content-type':'application/x-www-form-urlencoded;charset=UTF-8' },
+                   data='text=' + raw )
+
+    file.close()
+
+else:
+
+    print(soup.find('textarea', {"id": "text"}).text)
