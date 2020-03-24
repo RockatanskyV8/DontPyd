710ab3712ea26c5b80be22c412f2d9952960f887
RockatanskyV8 <RockatanskyV8@github.com>
2020-03-24 18:33:43 -0300

    mais organização

--- pad.py
+++ pad.py
@@ -3,38 +3,41 @@ import requests
 import os
 import sys
 
-args = sys.argv #[1]
-link = (args[-1].split('/'))
+EDITOR="nano"
 
-source = requests.get('http://dontpad.com/' + args[-1]).text
+args = sys.argv #[1]
+path = (args[-1].split('/'))
+
+link         = 'http://dontpad.com/' + args[-1]
+headers_post = { 'Content-type':'application/x-www-form-urlencoded;charset=UTF-8' }
+
+source = requests.get(link).text
 soup = BeautifulSoup(source, 'lxml')
 
 if args[1] == 'edit':
 
-    file = open(link[-1] + '.txt', 'w')
+    file = open(path[-1], 'w')
     file.write(soup.find('textarea', {"id": "text"}).text)
     file.close()
 
-    os.system("nano " + link[-1] + '.txt')
+    os.system('%s %s'%(EDITOR,path[-1]) )
 
-    file = open(link[-1] + '.txt', 'r')
+    file = open(path[-1], 'r')
     raw  = file.read()
 
-    requests.post('http://dontpad.com/' + args[-1],
-                   headers={ 'Content-type':'application/x-www-form-urlencoded;charset=UTF-8' },
-                   data='text=' + raw )
+    requests.post(link, headers=headers_post, data='text=' + raw)
 
     file.close()
 
-elif 'send:' in args[1]:
-    aux = (args[1]).split(':')[1]
+    os.remove(path[-1])
 
-    file = open(aux, 'r')
+elif 'send:' in args[1]:
+    sndFile = (args[1]).split(':')[1]
+
+    file = open(sndFile, 'r')
     raw  = file.read()
 
-    requests.post('http://dontpad.com/' + args[-1],
-                   headers={ 'Content-type':'application/x-www-form-urlencoded;charset=UTF-8' },
-                   data='text=' + raw )
+    requests.post(link, headers=headers_post, data='text=' + raw )
 
     file.close()
 
